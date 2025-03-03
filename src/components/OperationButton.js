62c75cf23e9132d964148f0a80e9a5fa46f90ada
Joey Souza <Souzajoey@gmail.com>
2025-03-02 21:41:24 -0500

    styles

--- src/components/OperationButton.js
+++ src/components/OperationButton.js
@@ -1,8 +1,9 @@
 import { ACTIONS } from "../App"
 
+
 export default function OperationButton({dispatch, operation}) {
     
-    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})} >
+    return <button className="operationBtn" onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation}})} >
         {operation}
         </button>
 }
